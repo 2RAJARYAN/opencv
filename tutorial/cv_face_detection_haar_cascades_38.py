283b7fe377902ed58f20bb2e19eed1ad3c5979bd
2RAJARYAN <rajabijit99+github@gmail.com>
2026-06-23 13:17:13 +0530

    update face detection harr cascaded

--- tutorial/cv_face_detection_haar_cascades_38.py
+++ tutorial/cv_face_detection_haar_cascades_38.py
@@ -6,7 +6,7 @@ def nothing():
     pass
 
 cap=cv2.VideoCapture(0) 
-face_cascade=cv2.CascadeClassifier("video\haarcascade_frontalface_default.xml")  #this is classifier
+face_cascade=cv2.CascadeClassifier("video\haarcascade_frontalface_default.xml")  #The XML file contains a pre-trained face detection model
 cv2.namedWindow("frame")
 # cv2.createTrackbar("scale","frame",11,20,nothing)
 cv2.createTrackbar("neighbours","frame",11,20,nothing)
@@ -18,11 +18,12 @@ while True:
     # scale=cv2.getTrackbarPos("scale","frame")
     neighbours=cv2.getTrackbarPos("neighbours","frame")
 
-    faces=face_cascade.detectMultiScale(gray,1.3,neighbours)
+    faces=face_cascade.detectMultiScale(gray,1.3,neighbours)  #image, scaling factor(This controls image pyramid scaling.), neighbours(Controls false positives.)
+
     # print(faces)
     for rect in faces:
         (x,y,w,h)=rect
-        frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2) #return [[x,y,w,h],[x,y,w,h]]
 
 
     cv2.imshow("frame",frame)
